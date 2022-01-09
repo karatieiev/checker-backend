0af7c0cbc0f82004eb98e6ddc5e6cc83bf2ff439
Git user <hlamina@ukr.net>
2022-01-09 22:07:39 +0200

    add employees route

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
 from flask import Flask
+from app.db import get_employees
 
-# create and configure the app
-from app.persist_manager import PersistManager
 
 app = Flask(__name__, instance_relative_config=True)
 
 
-# a simple page that says hello
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
 
 
-@app.route('/create')
+@app.route('/employees')
 def create():
-    # PersistManager.init(app.config)
-    return 'file has been created'
+    return get_employees()
 
-
-@app.route('/read')
-def read():
-    my_file = open("BabyFile.txt", "r")
-    file_content = my_file.read()
-    my_file.close()
-    return file_content
