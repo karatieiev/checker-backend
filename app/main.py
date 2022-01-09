0d7664fb31dce2d0239c08997ee896f316daef9b
Git user <hlamina@ukr.net>
2022-01-09 18:34:26 +0200

    remove setting app.config

--- app/main.py
+++ app/main.py
@@ -1,21 +1,9 @@
-import os
-
 from flask import Flask
 
 # create and configure the app
 from app.persist_manager import PersistManager
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
-)
-
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
 
 
 # a simple page that says hello
@@ -27,7 +15,6 @@ def hello():
 @app.route('/create')
 def create():
     # PersistManager.init(app.config)
-    return app.config.get('DATABASE')
     return 'file has been created'
 
 
@@ -37,8 +24,3 @@ def read():
     file_content = my_file.read()
     my_file.close()
     return file_content
-
-
-f = open("BabyFile.txt", "w+")
-f.write('AAAAAAA')
-f.close()
