8f735141fc7c5c055992341dfb15272df17e64d2
Git user <hlamina@ukr.net>
2022-06-13 00:16:03 +0300

    allow CORS for all domains

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import app.db as db
 import app.bl as bl
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 
 
 app = Flask(__name__, instance_relative_config=True)
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
