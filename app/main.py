023efa461487636f644d3df7e0851970c3af0dd3
Git user <hlamina@ukr.net>
2022-06-14 15:25:24 +0300

    update routs

--- app/main.py
+++ app/main.py
@@ -2,21 +2,44 @@ import app.db as db
 import app.bl as bl
 from flask import Flask
 from flask import request
-from flask import send_file
+from flask import render_template, send_from_directory
 from flask_cors import CORS
 import os
 
-
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__
+            , instance_relative_config=True
+            , static_folder='../web/static/js'
+            , template_folder='../web'
+            , static_url_path=''
+            )
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
-def root():
-    try:
-        return send_file(os.path.join('web', 'index.html'))
-    except Exception as e:
-        return str(e)
+@app.route('/')
+@app.route('/web/employees')
+def serve():
+    return render_template('index.html')
+
+
+@app.route('/static/js/<file>')
+def serve_js(file):
+    return send_from_directory('../web/static/js', file)
+
+
+@app.route('/static/css/<file>')
+def serve_css(file):
+    return send_from_directory('../web/static/css', file)
+
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory('../web', 'manifest.json')
+
+
+@app.route('/logo192.png')
+@app.route('/favicon.ico')
+def serve_logo():
+    return send_from_directory('../web', 'favicon.ico')
 
 
 @app.route('/hello', methods=['GET'])
@@ -52,4 +75,3 @@ def check():
 def report():
     if request.method == 'GET':
         return db.get_report()
-
